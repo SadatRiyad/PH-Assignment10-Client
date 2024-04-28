0299ce22616d13b71bf1f77a4c0693206eb2d98e
SadatRiyad <sadatriyad@students.diu.ac>
2024-04-28 12:58:53 +0600

    fixing bugs

--- src/Components/Banner2/Banner2.jsx
+++ src/Components/Banner2/Banner2.jsx
@@ -10,7 +10,7 @@ const Banner2 = () => {
             </div>
             <div data-aos="fade-left" data-aos-duration="600" data-aos-anchor-placement="top-bottom" data-aos-delay="500" className="card-body w-full lg:w-2/4 justify-center pl-1 lg:pl-8">
                 <p className="card-title font-normal text-xs text-slate-100 uppercase">BB-Artistry Team</p>
-                <h2 className="card-title mt-2 font-bold text-2xl md:text-3xl lg:text-3xl">EXPLORE THE ART OF <br /> PAINTING & DRAWING</h2>
+                <h2 className="card-title mt-2 font-bold text-2xl md:text-3xl lg:text-3xl">EXPLORE THE TEAM OF <br /> PAINTING & DRAWING</h2>
                 <p className="text-slate-50 text-base mt-4">Welcome to BB-Artistry, where creativity meets craftsmanship. Immerse yourself in our curated collection of exquisite paintings, drawings, and unique craft items. Whether you are looking to adorn your space or commission a custom piece, our team of talented artists is here to bring your vision to life. Contact us today to discuss your creative ideas and discover the perfect addition to your collection.</p>
                 <div className="card-actions justify-start">
                     <Link to="/contactUs" className="btn bg-blue text-white border-blue hover:bg-blue hover:text-white hover:border-blue mt-5 hover:-translate-y-1 transition-all duration-100">Contact Us</Link>
